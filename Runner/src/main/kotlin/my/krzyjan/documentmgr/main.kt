f304773aca94b06a5bf7ec55e9e508dd624de3af
chrisn <maflaja@gmail.com>
2023-01-02 15:29:51 +1100

    Added kodein as a dependency injection framework.

--- Runner/src/main/kotlin/my/krzyjan/documentmgr/main.kt
+++ Runner/src/main/kotlin/my/krzyjan/documentmgr/main.kt
@@ -2,9 +2,14 @@ package my.krzyjan.documentmgr
 
 import io.nacular.doodle.application.Modules
 import io.nacular.doodle.application.application
+import org.kodein.di.DI
 
 fun main() {
-    application(modules = listOf(Modules.FontModule, Modules.PointerModule)) {
+    application(modules = listOf(
+        Modules.FontModule, Modules.PointerModule,
+        DI.Module(name = "AppModule") {
+        }
+    )) {
         NewDocumentManager()
     }
 }
