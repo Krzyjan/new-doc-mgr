96bcc688e3d7a86fdadb010a3c4d1933e5696588
chrisn <maflaja@gmail.com>
2023-06-07 13:02:24 +1000

    Made the entity class more compliant with the JPA spec.

--- Backend/src/main/kotlin/my/krzyjan/documentmgr/model/Document.kt
+++ Backend/src/main/kotlin/my/krzyjan/documentmgr/model/Document.kt
@@ -1,14 +1,21 @@
 package my.krzyjan.documentmgr.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
+/*
+** Everything here is explicitly defined as open to confirm to JPA specification
+ */
 @Entity
 @Table(name = "document")
-class Document(
-    @Id @GeneratedValue var id: Long = 0,
-    var name: String = "",
-    val path: String = ""
+open class Document(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    open var id: Long = 0,
+
+    @Column(name = "name", nullable = false)
+    open var name: String = "",
+
+    @Column(name = "path", nullable = false)
+    open val path: String = ""
 )
