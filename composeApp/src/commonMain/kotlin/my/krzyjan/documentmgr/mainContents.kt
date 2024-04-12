fefb9b6bafca85b4370a8aa23c9ac5a443ed62fb
chrisn <maflaja@gmail.com>
2024-04-12 11:04:35 +1000

    Removed redundant class.

--- composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/mainContents.kt
+++ composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/mainContents.kt
@@ -1,21 +1,37 @@
 package my.krzyjan.documentmgr
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.input.key.*
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isShiftPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
@@ -82,7 +98,6 @@ internal fun Modifier.onKeyUp(key: Key, action: () -> Unit): Modifier =
         }
     }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun newDocument() {
     val model = remember { rootStore }
@@ -123,7 +138,6 @@ private fun newDocument() {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 internal fun Modifier.moveOnFocusTab() = composed {
     val focusManager = LocalFocusManager.current
     onPreviewKeyEvent {
--- composeApp/src/desktopMain/kotlin/my/krzyjan/documentmgr/main.kt
+++ composeApp/src/desktopMain/kotlin/my/krzyjan/documentmgr/main.kt
@@ -1,18 +1,35 @@
 package my.krzyjan.documentmgr
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import my.krzyjan.documentmgr.model.DocumentService
 import my.krzyjan.documentmgr.model.ExposedDocumentService
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
-import org.kodein.di.instance
 
 val di = DI {
-    bindSingleton <UserInterface> { UserInterface() }
     bindSingleton <DocumentService> { ExposedDocumentService() }
 }
 
-fun main() {
-    val ui: UserInterface by di.instance()
-
-    ui.invoke()
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Document Manager",
+        state = rememberWindowState(
+            position = WindowPosition(alignment = Alignment.Center)
+        ),
+    ) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MaterialTheme {
+                mainView()
+            }
+        }
+    }
 }
