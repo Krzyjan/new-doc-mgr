0e6ca58d15ea9714fe0ed6b99fcf3f87364251bc
chrisn <maflaja@gmail.com>
2025-02-22 18:17:51 +1100

    UI enhancements for delete dialog

--- composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/mainContents.kt
+++ composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/mainContents.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
@@ -18,6 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.sksamuel.hoplite.ConfigLoaderBuilder
@@ -94,10 +98,10 @@ fun DeleteDialog(
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                Column {
+                Row {
                     Button(
                         onClick = onDeleteItemConfirmed,
-                        modifier = Modifier.align(Alignment.Start)
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
                     ) {
                         Text("Yes")
                     }
@@ -106,9 +110,9 @@ fun DeleteDialog(
 
                     Button(
                         onClick = onDeleteItemCancelled,
-                        modifier = Modifier.align(Alignment.End)
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
                     ) {
-                        Text("No")
+                        Text("No", fontWeight = FontWeight(1000))
                     }
                 }
             }
