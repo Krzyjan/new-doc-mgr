dbed64b96d7d26975a8b5b93137d6d4a26282af9
chrisn <maflaja@gmail.com>
2025-03-07 17:13:56 +1100

    Simplified the common code for opening a file.

--- composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/FileOpenerModel.kt
+++ composeApp/src/commonMain/kotlin/my/krzyjan/documentmgr/FileOpenerModel.kt
@@ -1,25 +1,11 @@
 package my.krzyjan.documentmgr
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import java.io.File
 
 class FileOpenerModel {
     private val fileOpener = FileOpener()
-    private var fileUri by mutableStateOf("")
-        private set
-    private var mimeType by mutableStateOf("")
-        private set
-    private fun setFile(fileUri: String, mimeType: String) {
-        this.fileUri = fileUri
-        this.mimeType = mimeType
-    }
-    private fun openFile() {
-        fileOpener.openFile(fileUri, mimeType)
-    }
+
     fun handleFile(fileName: String) {
-        setFile(File(fileName).toURI().toString(), "application/pdf")
-        openFile()
+        fileOpener.openFile(File(fileName).toURI().toString(), "application/pdf")
     }
 }
